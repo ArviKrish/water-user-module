084e57afb0d34aa1122b9c629a15065bc2adffb5
Senthilmurugan <akharry100@gmail.com>
2018-08-05 00:30:29 +0530

    Test change

--- src/main/java/com/water/user/exceptions/PhoneNumberExistsException.java
+++ src/main/java/com/water/user/exceptions/PhoneNumberExistsException.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.HttpStatus;
 
 
-	@ResponseStatus(value=HttpStatus.BAD_REQUEST, reason="Phone number already registered with the system")
+	@ResponseStatus(value=HttpStatus.BAD_REQUEST, reason="Phone number is already registered with the system")
 	public class PhoneNumberExistsException extends RuntimeException {
 
 		  public PhoneNumberExistsException(String exception) {
