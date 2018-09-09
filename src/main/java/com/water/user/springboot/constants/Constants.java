aaf8312df0fb39e7d030f18be0a5784212717a41
Senthilmurugan <akharry100@gmail.com>
2018-09-09 13:16:10 +0530

    Comments added for error codes

--- src/main/java/com/water/user/springboot/constants/Constants.java
+++ src/main/java/com/water/user/springboot/constants/Constants.java
@@ -17,16 +17,15 @@ public class Constants {
 	public static final String ERROR_CODE_1002 = "1002";
 	public static final String ERROR_CODE_1003 = "1003";
 	public static final String ERROR_CODE_1004 = "1004";
-	public static final String RESPONSE_CODE_001 = "001";
-	public static final String RESPONSE_CODE_002 = "002";
-	public static final String ERROR_CODE_1100 = "1100";
-	public static final String ERROR_CODE_1101 = "1101";
-	public static final String ERROR_CODE_1102 = "1102";
-	public static final String ERROR_CODE_1103 = "1103";
-	public static final String ERROR_CODE_1104 = "1104";
-	public static final String ERROR_CODE_1105 = "1105";
-	public static final String ERROR_CODE_1106 = "1106";
-	public static final String ERROR_CODE_1107 = "1107";
+	public static final String RESPONSE_CODE_001 = "001";		  //Rekki has been completed. User is ready for Sign up.
+	public static final String RESPONSE_CODE_002 = "002";		  //Phone number is not available in the system
+	public static final String ERROR_CODE_1100 = "1100";		  //Default code for login errors
+	public static final String ERROR_CODE_1101 = "1101";          //Phone number is registered as Wahter User and active.
+	public static final String ERROR_CODE_1102 = "1102";		  //Phone number is registered as Wahter User and inactive.
+	public static final String ERROR_CODE_1103 = "1103";		  //Phone number is registered as Potential User and Rekki not cleared.
+	public static final String ERROR_CODE_1104 = "1104";		  //Phone number is registered as Interested User..
+	public static final String ERROR_CODE_1105 = "1105";		  //Phone number is registered as Potential User and Rekki cleared
+	public static final String ERROR_CODE_1106 = "1106";		  //Incorrect Credentials provided for Login
 
 
 }
